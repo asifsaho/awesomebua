36248b8d2e7b0f5664cc39d5af49971b3609179d
Asif Nawaz <asif.saho@gmail.com>
2016-07-23 03:04:14 +0600

    personal dashboard calendar date dynamicd

--- app/partials/personal-dashboard/personal.dashboard.controller.js
+++ app/partials/personal-dashboard/personal.dashboard.controller.js
@@ -3,13 +3,31 @@
 
     angular.module('awesomebua').controller('PersonalDashboardController', function ($scope) {
 
-        $scope.notificationAmount = 12;
+        $scope.notificationAmount = 12; // static notification amount for future development
 
-        $scope.status = 'I will be late today, please keep the door open';
+        $scope.status = 'I will be late today, please keep the door open'; // static status msg for future development
+
+        var isLeapYear = function( year ){ //leap year testing function
+            if (year % 4) {
+                if(year % 100 ){
+                    return (year % 400) ? false : true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        };
+
+        var currentDate = new Date(); // current date
+        var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; // all months
+
+        $scope.currentYear = currentDate.getFullYear(); // current year
+        $scope.currentMonth = monthNames[currentDate.getMonth()]; // current month name
 
         $scope.months = [
             { name : 'January',     days : 31 },
-            { name : 'February',    days : 28 },
+            { name : 'February',    days : isLeapYear($scope.currentYear) ? 29 : 28 },
             { name : 'March',       days : 31 },
             { name : 'April',       days : 30 },
             { name : 'May',         days : 31 },
@@ -20,8 +38,25 @@
             { name : 'October',     days : 31 },
             { name : 'November',    days : 30 },
             { name : 'December',    days : 31 }
-
         ];
+
+        var currentMonthDays = function (monthName){
+            var dayNum = 30;
+            $scope.months.forEach(function(month){
+               if(month.name == monthName) {
+                   dayNum = month.days;
+               }
+            });
+            return dayNum;
+        };
+
+        $scope.range = function(n) {
+            return new Array(n);
+        };
+
+        $scope.currentMonthDays = currentMonthDays($scope.currentMonth);
+
+        console.log(typeof $scope.currentMonthDays);
 
         $scope.dropdown = [
             {
