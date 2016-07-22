86eda0c25c8df5b06f3a4a48927a5ee5b2293107
Asif Nawaz <asif.saho@gmail.com>
2016-07-23 01:31:58 +0600

    personal dashboard page created

--- app/partials/personal-dashboard/personal.dashboard.controller.js
+++ app/partials/personal-dashboard/personal.dashboard.controller.js
@@ -2,21 +2,39 @@
     'use strict';
 
     angular.module('awesomebua').controller('PersonalDashboardController', function ($scope) {
-        $scope.title = 'This is title';
+
+        $scope.notificationAmount = 12;
+
+        $scope.status = 'I will be late today, please keep the door open';
+
+        $scope.months = [
+            { name : 'January',     days : 31 },
+            { name : 'February',    days : 28 },
+            { name : 'March',       days : 31 },
+            { name : 'April',       days : 30 },
+            { name : 'May',         days : 31 },
+            { name : 'June',        days : 30 },
+            { name : 'July',        days : 31 },
+            { name : 'August',      days : 31 },
+            { name : 'September',   days : 31 },
+            { name : 'October',     days : 31 },
+            { name : 'November',    days : 30 },
+            { name : 'December',    days : 31 }
+
+        ];
 
         $scope.dropdown = [
             {
-                "text": "notification",
-                "href": "#anotherAction",
-                "active": true
+                "text": "This is a long example notification",
+                "href": "javascript:;"
             },
             {
-                "text": "notification",
-                "href": "#anotherAction",
+                "text": "lorem ipsum dolor sit amet lorem ipsum dolor sit amet",
+                "href": "javascript:;"
             },
             {
-                "text": "notification",
-                "href": "#anotherAction",
+                "text": "lorem ipsum dolor sit amet lorem ipsum dolor sit amet",
+                "href": "javascript:;"
             }
         ];
     });
